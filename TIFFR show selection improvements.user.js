e3daa857ed0ddace14d44358547eee96aae6c4db
Paul V <thePaulV@users.noreply.github.com>
2026-08-12 22:13:17 -0400

    Update film selection tweaks to new 2026 tiffr front-end

--- TIFFR show selection improvements.user.js
+++ TIFFR show selection improvements.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name     TIFFR show selection improvements
 // @description Improves the film list. Scrolls the list to the current film and adds keyboard shortcuts to the films list. left = previous film, right = next film, enter;s = toggle favourite, ctr/cmd+enter;t = watch trailer, shift-enter;l = go to official tiff page.
-// @version  2
+// @version  3
 // @grant    none
-// @include  https://*.tiffr.com/shows/*
+// @include  https://*.tiffr.com/films/*
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require  https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.min.js?a4098
 // ==/UserScript==
@@ -18,39 +18,39 @@ $(function() {
   // Go to the next film by pressing the "right" arrow key
   Mousetrap.bind("right", function() {
     console.log("TIFFR+: Next film");
-    go(".next-film");
+    go("article nav>div:last-child a");
   });
    
   // Go to the previous film by pressing the "left" arrow key
   Mousetrap.bind("left", function() {
     console.log("TIFFR+: Previous film");
-    go(".prev-film");
+    go("article nav>div:first-child a");
   });
   
   // Watch the trailer
   Mousetrap.bind(["mod+enter", "mod+return", "t"], function() {
     console.log("TIFFR+: watch trailer");
-    go("div.film-image-container > a"); // used to watch the trailer       
+    go("div.relative a[rel=noopener]"); // used to watch the trailer       
   });
 
   // Follow the link to the tiff site
   Mousetrap.bind(["shift+enter", "shift+return", "l"], function() {
       console.log("TIFFR+: follow link to tiff site");
-      go("div.film-metadata dl.film-metadata__datum:last a")
+      go("div.grid:nth-child(5) > div:nth-child(16) > a:nth-child(1)")
   });
 
   
   // Favourite a film by pressing enter
   Mousetrap.bind(["enter", "return", "s"], function() {
     console.log("TIFFR+: Favouriting film");
-    let el = $(".hvr-pulse");
+    let el = $("form.w-full button");
     el.click();
   });
   
   // Google a film pressing G
   Mousetrap.bind("g", function() {
     console.log("TIFFR+: Googling film");
-    let title = $(".film__title").text()
+    let title = $("article h1").text()
     let google = "https://www.google.com/search?q="
     let target = `${google}${title} tiff`;
     location.assign(target)
